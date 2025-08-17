b05434c356e95ea7cfddf86deb6e9881888e5598
Joel <Joelsari19@gmail.com>
2025-08-17 13:37:10 -0400

    Fix the light and dark background bug, also added a hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
 
 const App = () => {
   const [theme, setTheme] = useState(
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <div className="dark:bg-black relative">
       <Navbar theme={theme} setTheme={setTheme} />
+      <Hero />
     </div>
   );
 };
